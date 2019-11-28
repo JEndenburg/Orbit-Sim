e03299cd89d70a9fac9e487540249eab631cfdda
Jeroen Endenburg <jendenburg@sogyo.nl>
2019-11-28 12:39:03 +0100

    Interconnected projects

---
+++ orbit-sim-web/src/main/java/nl/sogyo/jendenburg/orbitsim/web/OrbitSimInterface.java
@@ -0,0 +1,13 @@
+package nl.sogyo.jendenburg.orbitsim.web;
+
+import nl.sogyo.jendenburg.orbitsim.domain.*;
+
+public class OrbitSimInterface implements IWebsocketMessageReceiver 
+{
+
+	@Override
+	public void receiveMessage(String message) 
+	{
+	}
+
+}
